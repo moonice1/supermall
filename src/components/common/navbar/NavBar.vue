4326f427e6ea035dc31a34c24154745a7a963c59
moonice1 <ymoon1006@163.com>
2022-04-22 17:22:57 +0800

    首页导航栏和请求数据

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'NavBar'
+}
+</script>
+
+<style scoped>
+  .nav-bar{
+    display: flex;
+    line-height: 44px;
+    /* 当没有内容时，只写line-height撑不起来，此时要设置height；有内容时不加height也可以撑起来 */
+    /* height: 44px; */
+    /* background-color: aqua; */
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+  }
+
+  .left,.right{
+    width: 60px;
+    /* background-color:blueviolet; */
+  }
+
+  .center{
+    flex: 1;
+  }
+</style>
---
+++ src/network/request.js
@@ -0,0 +1,32 @@
+// 很可能会封装多个实例,所以不使用export default
+import axios from "axios"
+
+export function request(config) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    // baseURL:'http://152.136.185.210:7878/api/m5',
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+
+  // 2.axios的拦截器
+  //config是指回调函数传过来的参数，名称可以随意起，其实内容就是配置config
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+    console.log(err)
+  })
+
+
+  // 响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err)
+  })
+
+
+  // 3. 发送真正的网络请求
+  // 直接return instance(config) 因为它这个本身就是一个Promise
+  return instance(config)  
+}
