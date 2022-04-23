93c728d142afca14ed42c02ede86c1ec0009cdc6
moonice1 <ymoon1006@163.com>
2022-04-23 21:45:25 +0800

    首页-featureview和tabcontrol

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,30 +4,89 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="titles"></tab-control>
+
+<div> 
+  <ul>
+      <li>111</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>111</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>111</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>111</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+      <li>222</li>
+    </ul>
+    </div>
+   
   </div>
 
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
 
 import {getHomeMultidata} from 'network/home'
 
 export default {
   name:'Home',
   components:{
-    NavBar,
+    
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl
 
   },
   data(){
     return {
       banners:[],
       recommends:[],
-      
+      titles:['流行','新款','精选']
     }
   },
   created(){
@@ -42,8 +101,23 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: #fff;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  
+  z-index: 100;
+}
+
+.tab-control{
+  position: sticky;
+  top: 44px;
 }
 </style>
