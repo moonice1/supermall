00b614bfbcdeed7da08b2f854bdcb2c27ccdc456
moonice1 <ymoon1006@163.com>
2022-04-27 11:16:42 +0800

    吸顶效果

--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -2,7 +2,7 @@
   <swiper>
       <swiper-item v-for="item in banners">
         <a :href="item.link">
-          <img :src="item.image" alt="">
+          <img :src="item.image" alt="" @load="imageLoad">
           {{item.img}}
         </a>
       </swiper-item>
@@ -18,6 +18,11 @@ components:{
   Swiper, 
   SwiperItem
 },
+data(){
+  return{
+    isLoad:false
+  }
+},
 props: {
     banners: {
       type: Array,
@@ -26,6 +31,15 @@ props: {
       },
     },
   },
+
+  methods:{
+    imageLoad(){
+      if(!this.isLoad){
+        this.$emit('swiperImageLoad')
+        this.isLoad = true
+      }      
+    }
+  }
 
 }
 </script>
