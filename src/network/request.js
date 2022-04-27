f8cae85d97751d57e389c94858185f227cb186e6
moonice1 <ymoon1006@163.com>
2022-04-27 11:15:15 +0800

    修改

--- src/network/request.js
+++ src/network/request.js
@@ -5,7 +5,7 @@ export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
     
-    baseURL:'http://152.136.185.210:7878/api/hy66',
+    
     // baseURL: 'http://123.207.32.32:8000',
     timeout: 5000
   })
