fba18577742ba0c6dfc463caf9377b2aac4710b7
moonice1 <ymoon1006@163.com>
2022-04-28 15:18:19 +0800

    店铺信息和商品详情

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+    </scroll>
+    
   </div>
 </template>
 
@@ -10,23 +15,33 @@
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
 import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
 
-import {getDetail,Goods} from 'network/detail'
+import {getDetail,Goods,Shop} from 'network/detail'
+
+import Scroll from 'components/common/scroll/Scroll' 
 
 export default {
   name:'Detail',
   components:{
     DetailNavBar,
     DetailSwiper,
-    DetailBaseInfo
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    Scroll,
+    
   },
   data(){
     return{
       iid:null,
       topImages:null,
-      goods:{}
-    }
-  },
+      goods:{},
+      shop:{},
+      detailInfo:{}
+    }      
+    },
 
   created(){
     // 1.保存传入的iid
@@ -41,13 +56,38 @@ export default {
 
       // 2.获取商品信息
       this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
-    })
 
+      // 3.获取店铺信息的对象
+      this.shop = new Shop(data.shopInfo)
+
+      // 4.获取商品详细信息
+      this.detailInfo = data.detailInfo
+    })
   
+  },
+  methods:{
+    imageLoad(){
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
+#detail{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
 
+.detail-nav{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+
+.content{
+  height: calc(100% - 44px);
+}
 </style>
